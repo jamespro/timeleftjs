a7eb8b931f6f9b927a25810e25a6c6aae72c8883
James Prickitt <james@precision-websites.com>
2024-04-21 11:34:52 -0400

    added version to console

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { Temporal } from "temporal-polyfill";
 import { periods } from "./periods";
 import { eventsDataJSON } from "./eventsdata";
 // import "./style.css";
-
+console.log("%ctimeleft base10-20240421-1100", "background-color:lime");
 //For Fly.io, you can set environment variables through their web interface or CLI
 //const clientId = import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID;
 //const clientSecret = import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_SECRET;
